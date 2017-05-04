75eacf458c600fad59590b0409fbb0a673075197
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-05-04 13:52:08 -0600

    RoversTableViewController implemented

--- Sources/DMNListTableViewController.m
+++ Sources/DMNListTableViewController.m
@@ -7,92 +7,92 @@
 //
 
 #import "DMNListTableViewController.h"
+#import "DMNRover.h"
+#import "DMNMarsRoverClient.h"
+#import "DMNSolsTableViewController.h"
 
 @interface DMNListTableViewController ()
+
+@property (nonatomic, copy) NSArray *rovers;
 
 @end
 
 @implementation DMNListTableViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
+    NSMutableArray *rovers = [NSMutableArray array];
+    dispatch_group_t group = dispatch_group_create();
     
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    dispatch_group_enter(group);
+    DMNMarsRoverClient *client = [[DMNMarsRoverClient alloc] init];
+    [client fetchAllMarsRoversWithCompletion:^(NSArray *roverNames, NSError *error) {
+        if (error) {
+            NSLog(@"Error fetching list of mars rovers: %@", error);
+            return;
+        }
+        
+        dispatch_queue_t resultsQueue = dispatch_queue_create("com.devmountain.roverFetchResultsQueue", 0);
+        
+        for (NSString *name in roverNames) {
+            dispatch_group_enter(group);
+            [client fetchMissionManifestForRoverNamed:name completion:^(DMNRover *rover, NSError *error) {
+                if (error) {
+                    NSLog(@"Error fetching list of mars rovers: %@", error);
+                    dispatch_group_leave(group);
+                    return;
+                }
+                
+                dispatch_async(resultsQueue, ^{
+                    [rovers addObject:rover];
+                    dispatch_group_leave(group);
+                });
+            }];
+        }
+        
+        dispatch_group_leave(group);
+    }];
+    
+    dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
+    self.rovers = rovers;
 }
 
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
-}
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
+    return self.rovers.count;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"roverCell" forIndexPath:indexPath];
     
-    // Configure the cell...
+    cell.textLabel.text = [self.rovers[indexPath.row] name];
     
     return cell;
 }
-*/
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
 #pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([segue.identifier isEqualToString:@"ShowSolsSegue"]) {
+        DMNSolsTableViewController *destinationVC = segue.destinationViewController;
+        NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
+//        destinationVC.rover = self.rovers[indexPath.row]; Add this later.
+    }
 }
-*/
+
+#pragma mark - Properties
+
+- (void)setRovers:(NSArray *)rovers
+{
+    if (rovers != _rovers) {
+        _rovers = [rovers copy];
+        [self.tableView reloadData];
+    }
+}
 
 @end
